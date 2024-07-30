648314e47ea058f07ea4544a9262a58bbbe0302b
Den Williams <dennis.williams@loke.com.au>
2024-07-30 22:31:22 +1000

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # jemena-mqtt
 Scrapes usage metrics from Jemena and publishes over MQTT
+
+## Topics
+
+Emits `{prefix}/today` and `{prefix}/latest` with number data for today's running kWh usage, or latest kW per hour rate.
+
+## Running
+
+It is intended to be installed globally, ie `npm i -g jemena-mqtt`
+
+Create a YAML file somewhere. See `config.example.yml`
+
+Run (replace path)
+
+```
+CONFIG_PATH=./config.yml sunljemenaight-mqtt
+```
+
+You can also use Consul for config. See [mqtt-usvc](https://www.npmjs.com/package/mqtt-usvc) for more details.
+
+## Example Config
+
+```
+mqtt:
+  uri: mqtt://localhost
+  prefix: jemena
+service:
+  jemena_username: me@example.com
+  jemena_password: secret123
+  poll_interval: 1800000
+```
+
+## HTTP Status Endpoint
+
+Add port to config:
+
+```
+mqtt:
+  uri: mqtt://localhost
+  prefix: jemena
+http:
+  port: 9876
+service:
+  jemena_username: me@example.com
+  jemena_password: secret123
+  poll_interval: 1800000
+```
+
+Then request `http://localhost:9876/status`
+
+Metrics coming soon.
---
+++ main.ts
@@ -0,0 +1,107 @@
+import assert from "node:assert";
+import { readFileSync } from "node:fs";
+
+import { create } from "mqtt-usvc";
+import puppeteer from "puppeteer";
+import csv from "csvtojson";
+
+const pkg = JSON.parse(readFileSync("./package.json", "utf8"));
+
+type Config = {
+  poll_interval: number;
+  jemena_username: string;
+  jemena_password: string;
+};
+
+async function main() {
+  const service = await create<Config>();
+
+  const {
+    poll_interval = 1800000,
+    jemena_username,
+    jemena_password,
+  } = service.config;
+
+  assert(jemena_username, "jemena_username is required");
+  assert(jemena_password, "jemena_password is required");
+  assert(poll_interval, "poll_interval is required");
+
+  const handler = async () => {
+    // borrowed from https://github.com/ryanseddon/jemena-powershop/blob/master/src/index.js
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    const client = await page.createCDPSession();
+
+    await client.send("Page.setDownloadBehavior", {
+      behavior: "allow",
+      downloadPath: "./",
+    });
+    await page.goto("https://electricityoutlook.jemena.com.au/login/index");
+    await page.type("input#login_email", jemena_username);
+    await page.type("input#login_password", jemena_password);
+    await page.click("#loginButton");
+
+    //   await page.waitForNavigation();
+    await page.waitForSelector("#refresh-data");
+    //   await page.waitFor(5000);
+    await page.click("#refresh-data");
+    await page.waitForSelector("#fancybox-wrap", {
+      hidden: true,
+      timeout: 3000,
+    });
+    const usage = await page.evaluate(async () => {
+      const res = await fetch(
+        "https://electricityoutlook.jemena.com.au/electricityView/download",
+        { credentials: "include" }
+      );
+      return await res.text();
+    });
+
+    const rows = await csv().fromString(usage);
+    console.log(rows[0]);
+    let latest = rows[rows.length - 1];
+    //   console.log(latest);
+
+    const keys = Object.keys(latest);
+    if (keys[5] !== "00:00 - 00:30") {
+      throw new Error("Expected 00:00 - 00:30 to be at index 5");
+    }
+
+    let latestValues = Object.values(latest);
+    if (!latestValues[5]) {
+      latestValues = Object.values(rows[rows.length - 2]);
+    }
+
+    const values = latestValues
+      .slice(5)
+      .filter((item) => !!item)
+      .map(Number) as number[];
+
+    const total = Number(values.reduce((sum, value) => sum + value, 0)).toFixed(
+      2
+    );
+    const last = values.reduce((acc, value) => value || acc, 0);
+
+    // TODO: emit balancec
+    console.log("current total:", total);
+    console.log("latest kw:", last);
+
+    service.send("~/today", total); // total kwh for the day
+    service.send("~/latest", last * 2); // convert to kw per hour
+
+    await browser.close();
+  };
+
+  setInterval(handler, poll_interval);
+
+  handler();
+}
+
+main()
+  .then(() => {
+    console.log(`${pkg.name} ${pkg.version} started.`);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
